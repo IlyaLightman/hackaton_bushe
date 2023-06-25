cb000dbd70ac7ae385489d26ece235a7f7b06a6a
alexeymyakinin <alexeymyakinin@gmail.com>
2023-06-25 15:25:26 +0300

    courier.progress

--- backend/web/serializers/courier.py
+++ backend/web/serializers/courier.py
@@ -1,9 +1,13 @@
+import random
+
 from rest_framework import serializers
 
 from web.models import Courier
 
 
 class CourierSerializer(serializers.ModelSerializer):
+    progress = serializers.CharField(default=f"{random.randint(0, 50)} км")
+
     class Meta:
         model = Courier
         fields = "__all__"
