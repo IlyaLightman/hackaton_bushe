e661ca4038b13e6c4db2087178abc3792520d176
alexeymyakinin <alexeymyakinin@gmail.com>
2023-06-25 16:42:24 +0300

    update seed

--- backend/web/management/commands/seed.py
+++ backend/web/management/commands/seed.py
@@ -92,7 +92,7 @@ class Command(BaseCommand):
     def _create_waybills(self):
         for idx, (courier, status) in enumerate(zip(Courier.objects.all(), WaybillStatusChoices.names)):
             instance = Waybill.objects.create(
-                number=f"Маршрут №1432-{idx + 1}",
+                number=f"Маршрут {idx + 1}",
                 status=WaybillStatusChoices.created,
                 courier=courier,
             )
