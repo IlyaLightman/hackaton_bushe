2edc8f077d227084f0b91434ce2d793aa96ba709
alexeymyakinin <alexeymyakinin@gmail.com>
2023-06-25 14:16:15 +0300

    create waybill

--- backend/web/views/waybill.py
+++ backend/web/views/waybill.py
@@ -1,9 +1,13 @@
-from rest_framework import viewsets
+from django.db import transaction
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
 from web.models import Waybill
-from web.serializers.waybill import CreateWaybillSerializer, WaybillSerializer,GetWaybillSerializer
+from web.serializers.waybill import CreateWaybillSerializer, GetWaybillSerializer, WaybillSerializer
 
-class WaybillViewSet(viewsets.GenericViewSet):
+
+class WaybillViewSet(viewsets.ModelViewSet):
     queryset = Waybill.objects.all()
 
     def get_serializer_class(self):
@@ -14,3 +18,20 @@ class WaybillViewSet(viewsets.GenericViewSet):
         }
 
         return mapping.get(self.action, WaybillSerializer)
+
+    @swagger_auto_schema(
+        request_body=CreateWaybillSerializer,
+        responses={
+            status.HTTP_201_CREATED: WaybillSerializer(),
+        },
+    )
+    @transaction.atomic()
+    def create(self, request, *args, **kwargs):
+        request_serializer = CreateWaybillSerializer(data=request.data)
+        request_serializer.is_valid(raise_exception=True)
+
+        waybill = self.perform_create(request_serializer)
+
+        response_serializer = WaybillSerializer(waybill)
+
+        return Response(response_serializer.data, status=status.HTTP_201_CREATED)
