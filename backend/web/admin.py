b2d2ebca0272c6c72a6648112e2bd9b7b4e68885
Aleksandr Gavrilov <agvrdir@gmail.com>
2023-06-25 12:26:29 +0300

    add admin

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from web.models import *
+
 # Register your models here.
+admin.site.register(Hub)
+admin.site.register(Courier)
+admin.site.register(Order)
+admin.site.register(Waybill)
